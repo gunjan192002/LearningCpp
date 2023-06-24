a5e51d4f88048461269df1c533fe489fc447e7b2
Gunjan Agrawal <gunjan11223374@gmail.com>
2023-06-24 16:20:39 +0530

    morecode

--- Untitled-10.cpp
+++ Untitled-10.cpp
@@ -1,5 +1,7 @@
-//#include <bits/stdc++.h>
-
+// _GUNJAN_AGRAWAL_ 
+//insta id->_gunjan_agrawal_
+//Leetcode-> https://leetcode.com/gunjan192002/
+//LinekdIN->https://www.linkedin.com/in/gunjan-agrawal-537929229/
 #include <vector>
 #include <map>
 #include <algorithm>
@@ -8,11 +10,19 @@
 #include <queue>
 #include <set>
 #include <iostream>
-//F047_gunjan_agrawal
-//gunjan agrawal
-//insta id->_gunjan_agrawal_
 #define pi (3.141592653589)
-#define M 1e9+7
+#define M 1000000007
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define inint(x) int x; cin>>x;
+#define inll(x) long long int x; cin>>x;
+#define all(x) x.begin(), x.end()
+#define MAX(x) *max_element(all(x))
+#define MIN(x) *min_element(all(x))
+#define SUM(x) accumulate(all(x), 0LL)
+#define B break
+#define C continue
 #define ll long long int
 #define pb push_back
 #define mp make_pair
@@ -26,24 +36,16 @@
 #define ps(x,y) fixed<<setprecision(y)<<x
 #define pii pair<int,int>
 #define vi vector<int>
-
 using namespace std;
-
-void solve()
-{
-cout<<"hey";
-}
-
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+// I guess 2^n is the answer first calculate n ra
+//T P P P T T P T P T P P T T P T T P P P P P T T P T (12 D D P D)
+//P T T T P P P (15 P P)
+// P T T P T P T P T T P T P T T P T T T T T T T P P T P T P T P P T 
+//T P P T T T T T T T T T T P T P P P P T P T T
 
 int32_t main()
 {
-    fast
-    //cooldude69.
-    int t;
-    cin>>t;
-    while(t--)
-    {
-    solve();
-    }
-    return 0;
+  cout<<coloredCells(4);
 }
