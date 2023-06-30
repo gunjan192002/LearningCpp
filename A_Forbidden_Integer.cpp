80facee7f5879bd91046de29ce5563bce903d8e2
Gunjan Agrawal <gunjan11223374@gmail.com>
2023-07-01 04:50:22 +0530

    some more code

---
+++ A_Forbidden_Integer.cpp
@@ -0,0 +1,106 @@
+// _GUNJAN_AGRAWAL_ 
+//insta id->_gunjan_agrawal_
+//Leetcode-> https://leetcode.com/gunjan192002/
+//LinekdIN->https://www.linkedin.com/in/gunjan-agrawal-537929229/
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <set>
+#include <iostream>
+#define pi (3.141592653589)
+#define M 1000000007
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define inint(x) int x; cin>>x;
+#define inll(x) long long int x; cin>>x;
+#define all(x) x.begin(), x.end()
+#define MAX(x) *max_element(all(x))
+#define MIN(x) *min_element(all(x))
+#define SUM(x) accumulate(all(x), 0LL)
+#define B break
+#define C continue
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define f first
+#define s second
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repj(j,n) for(int j=0;j<n;j++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define pii pair<int,int>
+#define vi vector<int>
+using namespace std;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+
+void solve()
+{
+    int n,k,x;
+    cin>>n>>k>>x;
+    if(k==1){cout<<"NO"<<endl;return ;}
+     if(x==1)
+     {
+        if(n%2==0)
+        {
+                cout<<"YES"<<endl;
+                cout<<n/2<<endl;
+                rep(i,0,n/2)
+                {
+                    cout<<2<<" ";
+                }
+                cout<<endl;
+                return;
+        }
+        else 
+        {
+            if(k==2){cout<<"NO"<<endl;return;}
+            else{
+                int k=n-3;
+                if(k<0){cout<<"NO"<<endl;return;}
+
+                else{
+                    cout<<"YES"<<endl;
+                    cout<<(n-1)/2<<endl;
+                    cout<<3<<" ";
+                    rep(i,0,k/2)
+                    {
+                        cout<<2<<" ";
+                    }
+                    cout<<endl;
+                    return ;
+                }
+            }
+        }
+     }
+     else
+     {
+        cout<<"YES"<<endl;
+        cout<<n<<endl;
+        rep(i,0,n)
+        {
+            cout<<1<<" ";
+        }
+     }
+     cout<<endl;
+}
+
+
+int32_t main()
+{
+    fast
+    //_GUNJAN_AGRAWAL_
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    solve();
+    }
+    return 0;
+}
---
+++ B_Come_Together.cpp
@@ -0,0 +1,76 @@
+// _GUNJAN_AGRAWAL_ 
+//insta id->_gunjan_agrawal_
+//Leetcode-> https://leetcode.com/gunjan192002/
+//LinekdIN->https://www.linkedin.com/in/gunjan-agrawal-537929229/
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <set>
+#include <iostream>
+#define pi (3.141592653589)
+#define M 1000000007
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define inint(x) int x; cin>>x;
+#define inll(x) long long int x; cin>>x;
+#define all(x) x.begin(), x.end()
+#define MAX(x) *max_element(all(x))
+#define MIN(x) *min_element(all(x))
+#define SUM(x) accumulate(all(x), 0LL)
+
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define f first
+#define s second
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repj(j,n) for(int j=0;j<n;j++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define pii pair<int,int>
+#define vi vector<int>
+using namespace std;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+
+void solve()
+{
+    int xa,ya,xb,yb,xc,yc;
+    cin>>xa>>ya;
+    cin>>xb>>yb;
+    cin>>xc>>yc;
+    int A=abs(xa-xb);
+    int B=abs(ya-yb);
+    int C=abs(xc-xa);
+    int D=abs(yc-ya);
+    int ans=1;
+    if((xb>=xa&&xc>=xa)||(xb<xa&&xc<xa))
+    {
+        ans=ans+min(A,C);
+    }
+        if((yb>=ya&&yc>=ya)||(yb<ya&&yc<ya))
+    {
+        ans=ans+min(B,D);
+    }
+    cout<<ans<<endl;
+}
+
+
+int32_t main()
+{
+    fast
+    //_GUNJAN_AGRAWAL_
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    solve();
+    }
+    return 0;
+}
