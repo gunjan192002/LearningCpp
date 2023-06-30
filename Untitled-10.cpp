9f1e853248f03f78696f3594b1dd5f2e497da7b2
Gunjan Agrawal <gunjan11223374@gmail.com>
2023-07-01 05:00:25 +0530

    some mroe code

--- Untitled-10.cpp
+++ Untitled-10.cpp
@@ -1,61 +1,60 @@
-#include <iostream>
+// _GUNJAN_AGRAWAL_ 
+//insta id->_gunjan_agrawal_
+//Leetcode-> https://leetcode.com/gunjan192002/
+//LinekdIN->https://www.linkedin.com/in/gunjan-agrawal-537929229/
 #include <vector>
-#include <string>
-
+#include <map>
+#include <algorithm>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <set>
+#include <iostream>
+#define pi (3.141592653589)
+#define M 1000000007
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define inint(x) int x; cin>>x;
+#define inll(x) long long int x; cin>>x;
+#define all(x) x.begin(), x.end()
+#define MAX(x) *max_element(all(x))
+#define MIN(x) *min_element(all(x))
+#define SUM(x) accumulate(all(x), 0LL)
+#define B break
+#define C continue
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define f first
+#define s second
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repj(j,n) for(int j=0;j<n;j++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define pii pair<int,int>
+#define vi vector<int>
 using namespace std;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
 
-int main() {
-int t;
- cin >> t;
+void solve()
+{
 
-while (t > 0) {
- string s;
-cin >> s;
-
-vector<vector<int>> pos(10);
-vector<int> idx(10, 0);
-for (int i = 0; i < s.length(); i++) {
-int digit = s[i] - '0';
-pos[digit].push_back(i);
- }
-
-int m;
-cin >> m;
-string lr, rr;
-cin >> lr >> rr;
-int cpos = 0;
-
-for (int i = 0; i < m; i++) {
-for (int j = 0; j < 10; j++) {
- while (idx[j] < pos[j].size() && pos[j][idx[j]] < cpos) {
-idx[j]++;
-}
-}
-
-int pck = cpos;
-for (int j = lr[i] - '0'; j <= rr[i] - '0'; j++) {
-if (idx[j] >= pos[j].size()) {
-pck = s.length();
-} else {
-pck = max(pck, pos[j][idx[j]]);
- }
-}
-
-cpos = pck + 1;
-}
-
-if (cpos >= s.length() + 1) {
-cout << "YES" << endl;
-} else {
-cout << "NO" << endl;
- }
-
-t--;
-}
-
-    return 0;
 }
 
 
-
-C
+int32_t main()
+{
+    fast
+    //_GUNJAN_AGRAWAL_
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    solve();
+    }
+    return 0;
+}
