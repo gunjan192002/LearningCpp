b51519a014c186439dc0a6ca6eba116d7db9016b
Gunjan Agrawal <gunjan11223374@gmail.com>
2023-11-25 14:51:35 +0530

    practised some more code

--- trie implementation.cpp
+++ trie implementation.cpp
@@ -6,12 +6,46 @@ class Trie
     Trie *child[26];
     bool idEnd;
 };
-int main()
+Trie *getNode()
 {
 
-    Trie *child=new Trie();
+    Trie *root=new Trie();
     for(int i=0;i<26;i++)
     {
-        
+        root->child[i]=NULL;
+        root->idEnd=false;
     }
+    return root;
+}
+void insert(Trie *root,string word)
+{
+    Trie *temp=root;
+    for(int i=0;i<word.size();i++)
+    {
+        if(!temp->child[word[i]-'a'])
+        {
+            temp->child[word[i]-'a']=getNode();
+            temp=temp->child[word[i]-'a'];
+        }
+    }
+    temp->idEnd=true;
+}
+bool search(Trie *root,string word)
+{
+    Trie *temp=root;
+    for(int i=0;i<word.size();i++)
+    {
+        if(!temp->child[word[i]-'a'])
+        {
+           return false;
+        }
+        temp= temp->child[word[i]-'a'];
+    }
+    return temp->idEnd;
+}
+int main()
+{
+    Trie *root=new Trie();
+    insert(root,"gunjan");
+    cout<<search(root,"gunjan")<<endl;
 }
